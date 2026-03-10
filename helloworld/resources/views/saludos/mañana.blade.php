a093241709906f7454311158f73e6d4ef1f83ac4
Patricio Salazar <patricio.victor.2005@gmail.com>
2026-03-10 17:50:58 +0100

    Ejercicio 3 hecho, creado primer libro

--- helloworld/resources/views/saludos/mañana.blade.php
+++
@@ -1,8 +0,0 @@
-<body style="background-color: lightcyan;">
-<h1>🌞 {{ $text_salutacio }}</h1>
-<p>Benvingut al sistema. Són les {{ $hora_actual }}.</p>
-
-@if($es_mati)
-    <p>Recorda fer un bon cafè!</p>
-@endif
-</body>
