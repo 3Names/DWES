69f6c08bb60636e182eaf5746637389099af4f29
Patricio Salazar <patricio.victor.2005@gmail.com>
2026-05-05 20:18:12 +0200

    JS llamada a API

--- juegos/public/app.js
+++ juegos/public/app.js
@@ -1,4 +1,29 @@
+/*const llista = document.getElementById('llista-plataformas');
+
+// Funció per carregar categories
+async function carregarPlataformas() {
+    try {
+        const response = await fetch('/api/plataformas');
+        const plataformas = await response.json();
+
+        llista.innerHTML = '';
+        plataformas.forEach(cat => {
+            const li = document.createElement('li');*/
+//            li.textContent = cat.nombre; // 'name' és el nom del camp a la BD
+//            llista.appendChild(li);
+//        });
+//    } catch (error) {
+//        console.error("Error al carregar:", error);
+//    }
+//}
+//
+// Inicialització
+//carregarPlataformas();
+
 const llista = document.getElementById('llista-plataformas');
+// nous elements
+const input = document.getElementById('nom-plataforma');
+const btn = document.getElementById('btn-afegir');
 
 // Funció per carregar categories
 async function carregarPlataformas() {
@@ -9,7 +34,7 @@ async function carregarPlataformas() {
         llista.innerHTML = '';
         plataformas.forEach(cat => {
             const li = document.createElement('li');
-            li.textContent = cat.name; // 'name' és el nom del camp a la BD
+            li.textContent = cat.nombre; // 'name' és el nom del camp a la BD
             llista.appendChild(li);
         });
     } catch (error) {
@@ -17,5 +42,26 @@ async function carregarPlataformas() {
     }
 }
 
+// Nova funció per afegir categoria
+// Afegim una funció per l’event de clickar el botó del fomulari
+// Farem una petició POST, enviant el texte del camp de text
+btn.addEventListener('click', async () => {
+    const name = input.value;
+
+    await fetch('/api/plataformas', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({ nombre: name })
+    });
+
+    input.value = '';
+    carregarPlataformas(); // Actualitzem la llista immediatament
+});
+
 // Inicialització
 carregarPlataformas();
+
+setInterval(carregarPlataformas,10000);
